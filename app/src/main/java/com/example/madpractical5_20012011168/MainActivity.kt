2b7aef84da3a56eca5c863bca24ce68c832744b4
Ayush-M-Thakar <amthakar1512@gmail.com>
2022-11-14 15:18:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical5_20012011168/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.madpractical5_20012011168
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+import com.example.madpractical5_20012011168.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun play(view: View){
+        Intent(applicationContext,MyService::class.java)
+            .putExtra(MyService.DATA_KEY,MyService.DATA_VALUE)
+            .apply { startService(this) }
+        Toast.makeText(this, "Play is clicked", Toast.LENGTH_LONG).show()
+    }
+
+    fun stop(view:View){
+        Intent(applicationContext,MyService::class.java)
+            .apply { stopService(this) }
+        Toast.makeText(this, "Pause is clicked", Toast.LENGTH_LONG).show()
+
+
+    }
+}
